9b9fbab38221a5a076d9702aeb84f06720203de9
Trohodon <isfleshman@gmail.com>
2025-12-09 08:09:42 -0500

    Create tab_compare.py

---
+++ gui/tab_compare.py
@@ -0,0 +1,6 @@
+from tkinter import ttk
+
+class CompareTab(ttk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        ttk.Label(self, text="Comparison tools coming soon…").pack(pady=20)
