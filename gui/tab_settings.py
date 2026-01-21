58b3873cca30682af67315a4171216168261ccb0
Trohodon <isfleshman@gmail.com>
2026-01-21 11:44:47 -0500

    Delete gui/tab_settings.py

--- gui/tab_settings.py
+++
@@ -1,6 +0,0 @@
-from tkinter import ttk
-
-class SettingsTab(ttk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        ttk.Label(self, text="Settings tab (future features)").pack(pady=20)
